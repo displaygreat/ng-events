13bc8704e17e28829181a222bc337f38dafba478
displaygreat <displaygreat@gmail.com>
2021-06-10 12:22:26 +0300

    add service for events

---
+++ src/app/events.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface Event {
+  id: string;
+  name: string;
+  author: string;
+  date: string;
+  time: string;
+  counter: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEvents$(): Observable<Event[]> {
+    return this.httpClient.get<Event[]>('/api/getAllEvents')
+  }
+}
+
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Event } from '../app.component';
+// import { Event } from '../app.component';
+import { EventsService, Event } from '../events.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-events',
@@ -7,22 +9,28 @@ import { Event } from '../app.component';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
-  @Input()
-  event!: Event;
+  // @Input()
+  // event!: Event;
+  events$!: Observable<Event[]>;
 
-  constructor() {}
+  constructor(private eventsService: EventsService) { }
 
   ngOnInit(): void {
+    // this.events$ = this.eventsService.getEvents$();
   }
 
-  addTicket() {
-    if (this.event.counter <10) {
-    ++this.event.counter;
-    }    
-  }
-  removeTicket() {
-    if (this.event.counter !== 0) {
-    --this.event.counter;
-    }
+  // addTicket() {
+  //   if (this.event.counter <10) {
+  //   ++this.event.counter;
+  //   }    
+  // }
+  // removeTicket() {
+  //   if (this.event.counter !== 0) {
+  //   --this.event.counter;
+  //   }
+  // }
+
+  getEvents(){
+    this.events$ = this.eventsService.getEvents$();
   }
 }
