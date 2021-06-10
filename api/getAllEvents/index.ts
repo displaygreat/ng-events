09e743e57bed028f0b339bc75ffab8471cc61255
displaygreat <displaygreat@gmail.com>
2021-06-10 16:43:50 +0300

    try try try helloEvent

--- api/getAllEvents/index.ts
+++ api/getAllEvents/index.ts
@@ -2,15 +2,13 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { Events } from "../model";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-
+    context.res.json({
+    text: "Hello from Get All Events",
+    });
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: context.bindings.events
     };
-    context.res.json({
-    text: "Hello from Get All Events",
-    });
-    
 };
 
 export default httpTrigger;
