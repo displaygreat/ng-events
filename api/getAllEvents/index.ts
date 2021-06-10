fe6a35eb5fb85478060a20f039cbc0219ea66bd4
displaygreat <displaygreat@gmail.com>
2021-06-10 17:47:28 +0300

    try context.log

--- api/getAllEvents/index.ts
+++ api/getAllEvents/index.ts
@@ -2,9 +2,13 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { Events } from "../model";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('TypeScript queue trigger function processed');
+    context.log("Found Events" + context.bindings.events);
+    
     context.res.json({
     text: "Hello from Get All Events",
     });
+    context.done();
 
     // context.res = {
     //     // status: 200, /* Defaults to 200 */
