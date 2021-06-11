421d85b786b71c95585a7adfd022f66d2366d379
displaygreat <displaygreat@gmail.com>
2021-06-11 11:19:48 +0300

    without posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-export interface Event {
-  id: number;
-  name: string;
-  author: string;
-  date: string;
-  time: string;
-  counter: number;
-}
+// export interface Event {
+//   id: number;
+//   name: string;
+//   author: string;
+//   date: string;
+//   time: string;
+//   counter: number;
+// }
 
 @Component({
   selector: 'app-root',
@@ -19,19 +19,16 @@ export class AppComponent {
   title = 'NGTickets - Tickets Engine';
   organization = "HackaLearn";
   toggle = false;
-  events: Event[] = [
-    {id: 1, name: 'HackaLearn', author: 'AngularNights', date: "01/07/21", time: "08:00", counter: 0},
-    {id: 2, name: 'HackaLive', author: 'AngularNights', date: "02/07/21", time: "12:00", counter: 0},
-    {id: 3, name: 'HackaJoy', author: 'AngularNights', date: "03/07/21", time: "16:00", counter: 0},
-  ]
+  // events: Event[] = [
+  //   {id: 1, name: 'HackaLearn', author: 'AngularNights', date: "01/07/21", time: "08:00", counter: 0},
+  //   {id: 2, name: 'HackaLive', author: 'AngularNights', date: "02/07/21", time: "12:00", counter: 0},
+  //   {id: 3, name: 'HackaJoy', author: 'AngularNights', date: "03/07/21", time: "16:00", counter: 0},
+  // ]
   message = '';
-  // helloEvent = '';
 
   constructor(private http: HttpClient) {
     this.http.get('/api/message')
       .subscribe((resp: any) => this.message = resp.text);
-    // this.http.get('/api/getAllEvents')
-    //   .subscribe((resp: any) => this.helloEvent = resp.text);
   }
   
   toggleEvents () {
