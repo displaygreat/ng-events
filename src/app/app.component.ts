4766e615122a0082d03353af422ba56a92ea3470
displaygreat <displaygreat@gmail.com>
2021-06-16 12:27:55 +0300

    try catch errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-// export interface Event {
-//   id: number;
-//   name: string;
-//   author: string;
-//   date: string;
-//   time: string;
-//   counter: number;
-// }
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,11 +10,6 @@ export class AppComponent {
   title = 'ngTickets - Tickets Engine';
   organization = "HackaLearn";
   toggle = false;
-  // events: Event[] = [
-  //   {id: 1, name: 'HackaLearn', author: 'AngularNights', date: "01/07/21", time: "08:00", counter: 0},
-  //   {id: 2, name: 'HackaLive', author: 'AngularNights', date: "02/07/21", time: "12:00", counter: 0},
-  //   {id: 3, name: 'HackaJoy', author: 'AngularNights', date: "03/07/21", time: "16:00", counter: 0},
-  // ]
   message = '';
 
   constructor(private http: HttpClient) {
@@ -40,9 +26,5 @@ export class AppComponent {
     const response = await fetch("/api/getAllEvents");
     const payload = await response.json();     
     console.dir(payload);
-  }
-
-  logClick(e: any) {
-    console.log('title clicked', e);
   }
 }
